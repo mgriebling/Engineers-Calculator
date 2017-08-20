bad332ad99fc513926ab7831180fdeabc42e36c7
Michael Griebling <sam@c-inspirations.com>
2017-08-20 07:45:23 -0400

    Initial Commit

---
+++ Engineer's Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Engineer's Calculator
+//
+//  Created by Mike Griebling on 20 Aug 2017.
+//  Copyright © 2017 Computer Inspirations. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
